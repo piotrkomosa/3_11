d31e61c1ac89a9b77459a0f04a1f459ede9b1956
Piotr Komosa <komosa.piotr@gmail.com>
2019-11-04 19:11:10 +0100

    commit - excer3

---
+++ src/excer3/Array.java
@@ -0,0 +1,22 @@
+package excer3;
+
+public class Array {
+
+    public int N;
+    public Array(int n) {
+        N = n;
+    }
+    public int getN() {
+        return N;
+    }
+    public void setN(int n) {
+        N = n;
+    }
+
+
+    public int moveArray(int[]array, int N) {
+        for (int i = 0; i < array.length; i++) {
+        }
+        return N;
+    }
+}
---
+++ src/excer3/ArrayTest.java
@@ -0,0 +1,11 @@
+package excer3;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1,2,3,4,5};
+        int[] array2 = {6,7,8,9,10};
+
+    }
+}
